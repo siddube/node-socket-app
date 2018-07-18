17667831926e4144770deca94283de8671dbdff4
Prajwal Belagatti <prajwal.be.21@gmail.com>
2018-07-18 11:36:52 +0530

    Add custom socket.io events

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 var app = express();
@@ -12,12 +12,22 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-	console.log('New User Connected!');
-
-	socket.on('disconnect', () => {
-		console.log('User Disconnected');
+io.on("connection", socket => {
+	console.log("New User Connected!");
+	
+	socket.emit("newMessage", {
+		from: 'Gayathri',
+		text: 'Love you son!',
+		createdAt: 123456
 	});
+
+	socket.on("createMessage", (message) => {
+		console.log("Message Created", message);
+	});
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
 });
 
 server.listen(port, () => {
