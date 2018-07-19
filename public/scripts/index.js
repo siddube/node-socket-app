580340a126dd37f7c6e00e5fb2f567b5c9ebe935
Prajwal Belagatti <prajwal.be.21@gmail.com>
2018-07-19 12:38:56 +0530

    Add callbacks to socket event

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,17 @@
 var socket = io();
 socket.on("connect", function() {
   console.log("Connected To Server!");
-  /* socket.emit("createMessage", {
-    to: "Gayathri",
-    text: "Love you too, mother!"
-  }); */
 });
 socket.on("disconnect", function() {
   console.log("Disconnected From Server!");
 });
 socket.on("newMessage", function(message) {
   console.log("New Message", message);
+});
+
+socket.emit("createMessage", {
+  to: "Gayathri",
+  text: "Love you too, mother!"
+}, function (data) {
+  console.log('Got it!', data);
 });
--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ io.on("connection", socket => {
 
 	socket.broadcast.emit("newMessage", generateMessage('Admin', 'New User Joined'));
 
-	socket.on("createMessage", (message) => {
+	socket.on("createMessage", (message, callback) => {
 		console.log("Message Created", message);
 		io.emit("newMessage", generateMessage(message.from, message.text));
+		callback('This is from server');
 	});
 
   socket.on("disconnect", () => {
