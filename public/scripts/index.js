c15301046f4522439dc202bd0a914445099bab80
Prajwal Belagatti <prajwal.be.21@gmail.com>
2018-07-20 12:12:28 +0530

    Add Form and Message list on front end

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,11 +7,25 @@ socket.on("disconnect", function() {
 });
 socket.on("newMessage", function(message) {
   console.log("New Message", message);
+  var list = `
+  <li class="mdl-list__item">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-icon">person</i>
+      <span id="message-text">${message.text}</span>
+    </span>
+  </li>`;
+  $('#message-list').prepend(list);
 });
 
-socket.emit("createMessage", {
-  to: "Gayathri",
-  text: "Love you too, mother!"
-}, function (data) {
-  console.log('Got it!', data);
+$("#message-form").on("submit", function(e) {
+  e.preventDefault();
+  socket.emit("createMessage",{
+      from: "User",
+      text: $("#message-input").val()
+    },
+    function(data) {
+      console.log("Got it!", data);
+      $("#message-input").val(null);
+    }
+  );
 });
